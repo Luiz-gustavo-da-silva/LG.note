849d83684d2849bb7e0185eef725f7880615093b
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-04-25 13:01:48 -0300

    feat: ajuste no layout e implementação da função remover nota

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,21 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./index.module.css";
-import Note from "../Note/index"
+import Note from "../Note/index";
+import { Empty } from "antd";
 
 function Header() {
+  const [lista, setLista] = useState([
+    {
+      cor: "#dbbdff",
+      titulo: "teste 1",
+      mensagem: "teste 1",
+      id: 0,
+    },
+    {
+      cor: "#dbbdff",
+      titulo: "teste 2",
+      mensagem: "teste 2",
+      id: 1,
+    },
+    {
+      cor: "#dbbdff",
+      titulo: "teste 3",
+      mensagem: "teste 3",
+      id: 2,
+    },
+    {
+      cor: "#dbbdff",
+      titulo: "teste 4",
+      mensagem: "teste 4",
+      id: 3,
+    },
+  ]);
+
+
+  const removerNota = id => {
+    var novaLista=[];
+    novaLista = lista.filter(nota => nota.id !== id);
+    console.log(novaLista);
+    setLista(novaLista);
+  };
+
   return (
     <div className={style.mainContent}>
       <div className={style.divH1}>
         <h1>Notes</h1>
       </div>
       <div className={style.gridContainer}>
-        <Note cor={"#dbbdff"}/>
-        <Note cor={"#efeacd"}/>
-        <Note cor={"#efeacd"}/>
-        <Note cor={"#ffbdca"}/>
-        <Note cor={"#ffbdca"}/>
-        <Note cor={"#ddecd0"}/>
-        <Note cor={"#dbbdff"}/>
+        {lista.length > 0 ? (
+          lista.map((nota) => {
+            return (
+              <Note
+                key={nota.id}
+                cor={nota.cor}
+                titulo={nota.titulo}
+                mensagem={nota.mensagem}
+                id={nota.id}
+                removerNota={removerNota}
+              />
+            );
+          })
+        ) : (
+          <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+        )}
       </div>
     </div>
   );
--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import style from "./index.module.css";
 import { EditOutlined, CloseOutlined } from "@ant-design/icons";
 
-function Note({ cor }) {
+function Note({ cor, titulo, mensagem, id, removerNota }) {
+
   return (
     <div className={style.gridItem}>
       <div className={style.celula} style={{ background: `${cor}` }}>
         <div className={style.header}>
-          Título
-          <button
+          {titulo}
+          <button onClick={() => removerNota(id)}
             style={{
               background: "none",
               border: "none",
@@ -18,10 +19,10 @@ function Note({ cor }) {
               cursor: "pointer",
             }}
           >
-            <CloseOutlined style={{ fontSize: "0.7rem" }} />
+            <CloseOutlined  style={{ fontSize: "0.7rem" }} />
           </button>
         </div>
-        <div className={style.body}> Seu texto</div>
+        <div className={style.body}>{mensagem}</div>
         <div className={style.footer}>
           <button className={style.buttonPlus}>
             <EditOutlined />
